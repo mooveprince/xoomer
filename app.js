2cee8874d5377c065c24e230d3b7ffe2958b045a
mooveprince <mooveprince@gmail.com>
2014-12-03 23:13:45 -0600

    xoomerNewVersion

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
 var express = require('express');
-var CronJob = require('cron').CronJob;
-var nodemailer = require('nodemailer');
-var smtpPool = require('nodemailer-smtp-pool');
-var cheerio = require('cheerio'); 
-var request = require('request');
-var mustache = require ('mustache');
-var fs = require('fs');
 var path = require('path');
-var bodyParser = require('body-parser');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var async = require('async');
-
-var app = express();
-var scrape = require('./utility/xoomscraper');
-var sm = require('./utility/mailsender');
+var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 
+var app = express();
 //Port and View-engine setup
 app.set('port', process.env.PORT || 3000);
 
@@ -30,48 +19,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//DB Connection
-var pg = require('pg');
-var conString = process.env.DATABASE_URL;
-var client = new pg.Client(conString);
-client.connect();
-
-var rateChangeTemplate = process.cwd() + '/utility/rateChangeMail.html';
-var subscribedTemplate = process.cwd() + '/utility/subscribed.html';
-
-// Make our sockets accessible to our router
-app.use(function(req,res,next){
-    req.client = client;
-    req.async = async;
-    req.nodemailer = nodemailer;
-    req.smtpPool = smtpPool;
-    req.fs = fs;
-    req.subscribedTemplate = subscribedTemplate;
-    req.mustache = mustache;
-    req.sm = sm;
-    next();
-});
 app.use('/', routes);
 
-
-var job = new CronJob({
-  cronTime: '0 * * * * *',
-  onTick: function() {
-    console.log ("Runs every Minute..!!!");
-    scrape.checkXoomValue (request, cheerio, client, function ( ) {
-        sm.sendMail (nodemailer, smtpPool, client, fs, rateChangeTemplate, mustache, false, null);
-    });  
-  },
-  start: false,
-});
-
-//Uncomment below line for cron execution
-job.start();
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,154 +1,82 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
+var url = process.env.SERVICE_URL || "http://localhost:3003/services";
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', {option:'Subscribe', action:"/addSubscription"});
+  res.render('index',{option:'Subscribe', action:"/addSubscriber"});
 });
 
-router.get('/addSubscription', function(req, res) {
-    res.render('index'  , {option:'Subscribe', action:"/addSubscription"});
+router.get('/addSubscriber', function(req, res) {
+  res.render('index',{option:'Subscribe', action:"/addSubscriber"});
 });
 
 router.get('/removeSubscription', function(req, res) {
     res.render('index', {option:'Unsubscribe',action:"/removeSubscription"});
 });
 
-router.get('/terms', function(req, res) {
-    res.render('terms');
-});
-
-router.post('/removeSubscription', function (req, res) {
-    var client = req.client;
-    var alreadySubscribed = false;
-    var renewedSubscribed = false;
-    var newlySubscribed = false;
-    var async = req.async;
+router.post('/addSubscriber', function (req, res) {
+    var userDetailUrl = url + '/subscriberDetail';
+    var addUrl = url+'/addSubscriber';
+    var reactivateUrl = url + '/reactivatateSubscription' ;
     
-    client.query('select email, subscribed from xoomercustomer', function (err, result) {
-        if (err) { 
-            console.error(err); 
-        } else {
-            checkForSubscription (async, result, req.body.email, function (alreadySubscribed, renewedSubscribed) {    
-                console.log ( "OL " + alreadySubscribed + " RN " + renewedSubscribed );
-                if (alreadySubscribed) {
-                    alreadyActiveSubscribedHandler (res, false, true, client, req);
-                } else if (renewedSubscribed) {
-                    renewedSubscribedHandler (res, false, true, client, req);
-                } else {
-                    newlySubscribedHandler (res, false, true, client, req);
-                }
-            });
-        }
-    });
-    
-});
-
-router.post ('/addSubscription', function (req, res) {
-    //If new email, display thanks for subscription
-    var client = req.client;
-    var alreadySubscribed = false;
-    var renewedSubscribed = false;
-    var newlySubscribed = false;
-    var async = req.async;
-    
-    client.query('select email, subscribed from xoomercustomer', function(err, result) {
-    if (err) { 
-        console.error(err); 
-    }
-    else { 
-          console.log ("Before calling the checkSubscrib");
-          checkForSubscription (async, result, req.body.email, function (alreadySubscribed, renewedSubscribed) {
-              console.log ( "OL " + alreadySubscribed + " RN " + renewedSubscribed );
-              if (alreadySubscribed) {
-                    alreadyActiveSubscribedHandler (res, true, false, client, req);
-              } else if (renewedSubscribed) {
-                    renewedSubscribedHandler (res, true, false, client, req);
-              } else {
-                    newlySubscribedHandler (res, true, false, client, req);
-              }
-            });
-        }
-    });
-});
-
-checkForSubscription = function (async, result, email, callback ) {
-    if (result.rows.length == 0) {
-        callback (false, false);
-    } else {
-        var results = {
-            alreadySubscribed :false,
-            renewedSubscribed :false   
-        }
-        
-        async.map (result.rows,
-                    function (row, doneCallback) {
-                        if (email == row.email && row.subscribed == 'Y') {
-                            console.log ("Already subscribed");
-                            results.alreadySubscribed = true;
-                        } else if (email == row.email && row.subscribed == 'N') {
-                            console.log ("Renewed Subscribe");
-                            results.renewedSubscribed = true;
+    request.post ( {url:userDetailUrl, form: {email:req.body.email}, json:true },
+                  function (err, data, body) {
+                    if (err) {
+                        res.render('index',{option:'Subscribe', action:"/addSubscriber", message: "Some error occured, try again later !" });
+                    } else if (body.subscriberDetail.existingUser) {    //check whether he is existing user
+                        if (body.subscriberDetail.subscriptionStatus == 'Y') {  //check whether active user
+                            res.render('index', {message: "You are already subscribed !", option:'Subscribe', action:"/addSubscriber"});
+                        } else {
+                            request.post( {url:reactivateUrl, form: {email:req.body.email}, json:true }, 
+                                       function (err, data, body) {
+                                        if (body.reactivated) {
+                                            res.render('index',{option:'Subscribe', action:"/addSubscriber", message: "Thanks for subscribing again !" });                
+                                        } else {
+                                            res.render('index',{option:'Subscribe', action:"/addSubscriber", message: "Some error occured, try again later !" });                                
+                                        }     
+                                });                             
                         }
-                        doneCallback (null,results);   
-                    }, 
-                    function (err, results) { 
-                        console.log ("Inside callback..");
-                        callback ( results[0].alreadySubscribed, results[0].renewedSubscribed );
-                    } );
-                            
-    }
-}
+                    } else {
+                        request.post( {url:addUrl, form: {email:req.body.email}, json:true }, 
+                                   function (err, data, body) {
+                                    if (body.inserted) {
+                                        res.render('index',{option:'Subscribe', action:"/addSubscriber", message: "You will receive notification whenever the rate changes !" });                
+                                    } else {
+                                        res.render('index',{option:'Subscribe', action:"/addSubscriber", message: "Some error occured, try again later !" });                                
+                                    }     
+                            });                        
+                    }
+                  });
+});
 
-alreadyActiveSubscribedHandler = function (res, subscription, unsubscription, client, req) {
-    if (subscription) {
-        res.render('index', {message: "You are already subscribed !", option:'Subscribe',action:"/addSubscription"});
-    }
-    if (unsubscription) {
-        client.query('update xoomercustomer set subscribed = \'N\', unjoined = $1 where email= $2', [new Date( ), req.body.email], function (err, result) { 
-            if (err) {
-                console.log ("Err in update " + err);
-                res.render('index', {message: "Some error occured, try again later !", option:'Unsubscribe', action:"/removeSubscription"});                            
-            } else {
-                res.render('index', {message: "Sorry to see you leaving, you can subscribe back by entering again !", option:'Subscribe', action:"/addSubscription"});                            
-            }
-        });        
-    }
-}
-
-renewedSubscribedHandler = function (res, subscription, unsubscription, client, req) {
-    if (subscription) {
-        client.query ('update xoomercustomer set subscribed = \'Y\', rejoined = $1 where email= $2', [new Date( ), req.body.email], function (err, result) { 
-            if (err) {
-                console.log ("Error in update " + err);
-                res.render('index', {message: "Some error occured, try again later !", option:'Subscribe', action:"/addSubscription"});
-            } else {
-                res.render('index', {message: "Thanks for subscribing again !",option:'Subscribe',action:"/addSubscription"});
-            }
-        });          
-    }
-    if (unsubscription) { 
-        res.render('index', {message: "You are already unsubscribed, you can subscribe back by entering again !", option:'Subscribe', action:"/addSubscription"});        
-    }
-}
-
-newlySubscribedHandler = function (res, subscription, unsubscription, client, req) {
-    console.log ("Inside New mehtod");
-    if (subscription) {
-        client.query('insert into xoomercustomer (email, subscribed) values ($1, \'Y\')',[req.body.email],function (err, result) {
-            if (err) {
-                console.log ("Error in Create " + err);
-                res.render('index', {message: "Some error occured, try again later !", option:'Subscribe', action:"/addSubscription"});
-            } else {
-                req.sm.sendMail (req.nodemailer, req.smtpPool, req.client, req.fs, req.subscribedTemplate, req.mustache, true, req.body.email);
-                res.render('index', {message: "You will receive notification whenever the rate changes !", option:'Subscribe', action:"/addSubscription"});
-            }
-        });        
-    }
-    if (unsubscription) {
-        res.render('index', {message: "You haven't subscribed at all, you can subscribe newly above !", option:'Subscribe', action:"/addSubscription"});        
-    }
+router.post ('/removeSubscription', function (req, res) { 
+    var userDetailUrl = url + '/subscriberDetail';
+    var deactivateUrl = url + '/deactivatateSubscription';
     
-}
+    request.post ( {url:userDetailUrl, form: {email:req.body.email}, json:true },
+                  function (err, data, body) {
+                    if (err) {
+                        res.render('index',{option:'Unsubscribe', action:"/removeSubscription", message: "Some error occured, try again later !" });
+                    } else if (body.subscriberDetail.existingUser) { //check for existing user
+                        if (body.subscriberDetail.subscriptionStatus == 'Y') { //check if they are actively subscribed
+                            request.post( {url:deactivateUrl, form: {email:req.body.email}, json:true }, 
+                                       function (err, data, body) {
+                                        if (body.removed) {
+                                            res.render('index',{option:'Subscribe', action:"/addSubscriber", message: "Sorry to see you leaving, you can subscribe back by entering again !" });                
+                                        } else {
+                                            res.render('index',{option:'Unsubscribe', action:"/removeSubscription", message: "Some error occured, try again later !" });                                
+                                        }     
+                                });                                                    
+                        } else {
+                            res.render('index',{option:'Subscribe', action:"/addSubscriber", message: "You are already unsubscribed, you can subscribe back by entering again !" });                          
+                        }
+                    } else {
+                        res.render('index',{option:'Subscribe', action:"/addSubscriber", message: "You haven't subscribed at all, you can subscribe newly above !" });     
+                    }
+        });
+    
+});
 
 module.exports = router;
