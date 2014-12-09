6ca9b3673c3ed5f1c95a38ea6ec5ab14c4c34449
mooveprince <mooveprince@gmail.com>
2014-12-08 19:37:09 -0600

    addadded

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,15 @@ var url = process.env.SERVICE_URL || "http://localhost:3003/services";
 /* GET home page. */
 router.get('/', function(req, res) {    
     //sm.rateChangedMail (url);
-  res.render('index',{option:'Subscribe', action:"/addSubscriber"});
+  res.render('index',{option:'Add', action:"/addSubscriber"});
 });
 
 router.get('/addSubscriber', function(req, res) {
-  res.render('index',{option:'Subscribe', action:"/addSubscriber"});
+  res.render('index',{option:'Add', action:"/addSubscriber"});
 });
 
 router.get('/removeSubscription', function(req, res) {
-    res.render('index', {option:'Unsubscribe',action:"/removeSubscription"});
+    res.render('index', {option:'remove',action:"/removeSubscription"});
 });
 
 router.get('/terms', function(req, res) {
@@ -30,18 +30,18 @@ router.post('/addSubscriber', function (req, res) {
     request.post ( {url:userDetailUrl, form: {email:req.body.email}, json:true },
                   function (err, data, body) {
                     if (err) {
-                        res.render('index',{option:'Subscribe', action:"/addSubscriber", message: "Some error occured, try again later !" });
+                        res.render('index',{option:'Add', action:"/addSubscriber", message: "Some error occured, try again later !" });
                     } else if (body.subscriberDetail.existingUser) {    //check whether he is existing user
                         if (body.subscriberDetail.subscriptionStatus == 'Y') {  //check whether active user
-                            res.render('index', {message: "You are already subscribed !", option:'Subscribe', action:"/addSubscriber"});
+                            res.render('index', {message: "You are already subscribed !", option:'Add', action:"/addSubscriber"});
                         } else {
                             request.post( {url:reactivateUrl, form: {email:req.body.email}, json:true }, 
                                        function (err, data, body) {
                                         if (body.reactivated) {
-                                            res.render('index',{option:'Subscribe', action:"/addSubscriber", message: "Thanks for subscribing again !" }); 
+                                            res.render('index',{option:'Add', action:"/addSubscriber", message: "Thanks for subscribing again !" }); 
                                             sm.sendSubscriptionMail (url, req.body.email);
                                         } else {
-                                            res.render('index',{option:'Subscribe', action:"/addSubscriber", message: "Some error occured, try again later !" });                                
+                                            res.render('index',{option:'Add', action:"/addSubscriber", message: "Some error occured, try again later !" });                                
                                         }     
                                 });                             
                         }
@@ -49,10 +49,10 @@ router.post('/addSubscriber', function (req, res) {
                         request.post( {url:addUrl, form: {email:req.body.email}, json:true }, 
                                    function (err, data, body) {
                                     if (body.inserted) {
-                                        res.render('index',{option:'Subscribe', action:"/addSubscriber", message: "You will receive notification whenever the rate changes !" }); 
+                                        res.render('index',{option:'Add', action:"/addSubscriber", message: "You will receive notification whenever the rate changes !" }); 
                                         sm.sendSubscriptionMail (url, req.body.email);
                                     } else {
-                                        res.render('index',{option:'Subscribe', action:"/addSubscriber", message: "Some error occured, try again later !" });                                
+                                        res.render('index',{option:'Add', action:"/addSubscriber", message: "Some error occured, try again later !" });                                
                                     }     
                             });                        
                     }
@@ -66,22 +66,22 @@ router.post ('/removeSubscription', function (req, res) {
     request.post ( {url:userDetailUrl, form: {email:req.body.email}, json:true },
                   function (err, data, body) {
                     if (err) {
-                        res.render('index',{option:'Unsubscribe', action:"/removeSubscription", message: "Some error occured, try again later !" });
+                        res.render('index',{option:'Remove', action:"/removeSubscription", message: "Some error occured, try again later !" });
                     } else if (body.subscriberDetail.existingUser) { //check for existing user
                         if (body.subscriberDetail.subscriptionStatus == 'Y') { //check if they are actively subscribed
                             request.post( {url:deactivateUrl, form: {email:req.body.email}, json:true }, 
                                        function (err, data, body) {
                                         if (body.removed) {
-                                            res.render('index',{option:'Subscribe', action:"/addSubscriber", message: "Sorry to see you leaving, you can subscribe back by entering again !" });                
+                                            res.render('index',{option:'Add', action:"/addSubscriber", message: "Sorry to see you leaving, you can subscribe back by entering again !" });                
                                         } else {
-                                            res.render('index',{option:'Unsubscribe', action:"/removeSubscription", message: "Some error occured, try again later !" });                                
+                                            res.render('index',{option:'Remove', action:"/removeSubscription", message: "Some error occured, try again later !" });                                
                                         }     
                                 });                                                    
                         } else {
-                            res.render('index',{option:'Subscribe', action:"/addSubscriber", message: "You are already unsubscribed, you can subscribe back by entering again !" });                          
+                            res.render('index',{option:'Add', action:"/addSubscriber", message: "You are already unsubscribed, you can subscribe back by entering again !" });                          
                         }
                     } else {
-                        res.render('index',{option:'Subscribe', action:"/addSubscriber", message: "You haven't subscribed at all, you can subscribe newly above !" });     
+                        res.render('index',{option:'Add', action:"/addSubscriber", message: "You haven't subscribed at all, you can subscribe newly above !" });     
                     }
         });
     
